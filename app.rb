e65c641063db209af222cd544ceb0f3c5f1729bc
andrey678 <drew6496@yandex.ru>
2020-06-29 21:31:58 +0300

    Parse orders input

--- app.rb
+++ app.rb
@@ -21,5 +21,30 @@ get '/about' do
 	erb :about
 end
 post "/cart" do
-	erb
-	end
+	orders_input = params [:orders]
+	@orders = parse_orders_input orders_input
+	erb "Hello"
+end
+
+def parse_orders_input orders_input
+	s1 = orders_input.split (/,/) #здесь у нас получается массив s1 = ["product_1=2","product_2=2","product_3=3"]
+#puts s1.inspect
+	arr = []
+
+	s1.each do |x|
+        	s2 = x.split(/\=/)   # здесь у нас получается массив массивов s2 = ["product_1","2"],["product_2","2"],["product_3","3"] 
+		
+		s3 = s2[0].split(/_/) # здесь у нас получается массив массивов s3 = ["product","1"],["product","2"],["product","3"]
+		
+		id = s3[1]
+		cnt = s2[1]
+
+		arr2 = [id, cnt]
+		arr.push arr2
+end
+
+return arr
+#key = s3[1]
+#value = s2[1]
+#puts "Product id #{key}, number of items: #{value}"
+end
